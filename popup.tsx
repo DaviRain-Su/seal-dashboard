b75bd45f4111e8abac82991ee576ece884dea230
Davirain <davirain.yin@gmail.com>
2024-07-24 09:17:47 +0800

    update

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,24 @@
 import { useState } from "react"
 
+import "./style.css" // 我们将创建这个文件来存放CSS
+
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [count, setCount] = useState(0)
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="container">
+      <h1 className="title">Hello World!</h1>
+      <p className="subtitle">Welcome to your beautiful Plasmo Extension!</p>
+      <div className="counter">
+        <button className="button" onClick={() => setCount(count - 1)}>
+          -
+        </button>
+        <span className="count">{count}</span>
+        <button className="button" onClick={() => setCount(count + 1)}>
+          +
+        </button>
+      </div>
+      <p className="info">You've clicked {count} times</p>
     </div>
   )
 }
